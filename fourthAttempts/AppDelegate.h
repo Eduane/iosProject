2c02bab1721903d719f60bbb3eb722e54c2e69e0
Eduana <eduana@Eduanas-iMac.local>
2018-06-28 03:06:07 -0700

    Initial Commit

---
+++ fourthAttempts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fourthAttempts
+//
+//  Created by Eduana on 6/28/18.
+//  Copyright © 2018 Eduana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fourthAttempts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fourthAttempts
+//
+//  Created by Eduana on 6/28/18.
+//  Copyright © 2018 Eduana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
